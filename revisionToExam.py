e2732ea881ec434e00e7cc7e2a05f35f8ac0e009
yousefeslam <email@example.com>
2023-05-13 10:40:54 +0200

    finish

--- revisionToExam.py
+++ revisionToExam.py
@@ -68,3 +68,119 @@ c = 5;
 
 
 #####9-Python Program to Convert Celsius To Fahrenheit
+# cel = 37.5
+
+# fah =(cel * 1.8) + 32
+# print('%0.1f cel = %0.1f fah '%(cel,fah))
+
+
+#####10- Python Program to Check if a Number is Positive, Negative or 0
+# num = float(input("Enter a number: "))
+
+# if num > 0:
+#        print("Positive")
+# elif num < 0:
+#        print("Negative")
+# else:
+#        print("Zero")
+
+#####11- Python Program to Check if a Number is Odd or Even
+
+# num = int(input("Enter a Number: "))
+
+# if (num % 2) == 0 :
+#        print("{0} is Even".format(num))
+# else :
+#        print("{0} is Odd".format(num))
+
+
+######12- Python Program to Check Leap Year
+
+######13- Python Program to Find the Largest Among Three Numbers
+
+######14- Python Program to Check Prime Number
+
+# num = 10
+
+# if num > 1:
+#        for i in range(2,num):
+#               if (num % i) == 0:
+#                      print(num,"is not a prime number")
+#                      print(i,"times",num/i,"is",num)
+#                      break
+#        else :
+#                      print(num,"is a prime number")
+# else :
+#        print(num,"is not a prime number")
+
+
+######15- Python Program to Print all Prime Numbers in an Interval
+
+######16- Python Program to Find the Factorial of a Number
+# num = 4
+
+# factorial = 1
+
+# if num < 0:
+#        print("sorry, don't exist")
+# elif num == 0:
+#        print("the factoial of 0 is 1")
+# else :
+#        for i in range(1,num+1):
+#               factorial =factorial*i
+#        print("The factorial of",num,"is",factorial)
+
+
+#####17- Python Program to Display the multiplication Table
+
+#####18- Python Program to Print the Fibonacci sequence#####################################
+
+# nterms = int(input("How many terms? "))
+
+# n1 ,n2 = 0,1
+# count = 0
+
+# if nterms <= 0:
+#        print("Please enter a poitive integer")
+# elif nterms == 1:
+#        print("fibonacci sequence upto",nterms,":")
+#        print(n1)
+# else:
+#        print("Fibonacci sequence:")
+#        while count < nterms :
+#               print(n1)
+#               nth = n1 + n2
+#               n1 =n2
+#               n2 =nth
+#               count += 1
+
+
+#####19- Python Program to Check Armstrong Number############################
+
+#####20- Python Program to Find Armstrong Number in an Interval###############
+
+#####21- Python Program to Find the Sum of Natural Numbers ###########################
+
+# num = 4
+
+# if num < 0:
+#        print("enter + number")
+# else :
+#        sum = 0
+       
+#        while(num>0):
+#               sum += num
+#               num -= 1
+#        print("The sum is", sum)
+
+
+#####22- Python Program To Display Powers of 2 Using Anonymous Function
+
+
+# terms = 10 
+
+# res = list(map(lambda x: 2**x,range(terms)))
+
+# print("The total terms are :",terms)
+# for i in range(terms):
+#        print("2 raised to power ",i,"is",res[i])
